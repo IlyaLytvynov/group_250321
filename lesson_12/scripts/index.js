7f7c4d11dcfc7d82e467fd9c00b2bc0dc6287ef7
Illia Lutvynov <illial@wix.com>
2021-05-18 21:58:10 +0300

    Lesson 12 start

---
+++ lesson_12/scripts/index.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body');
+const header = document.createElement('h1');
+header.textContent = 'Hello Ilya!';
+header.classList.add('title');
+header.style.color = 'red';
+body.append(header);
